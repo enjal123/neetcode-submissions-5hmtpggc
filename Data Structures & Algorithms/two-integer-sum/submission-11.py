04a1a8e8a9858abcd5408d142ca582946d120d4c
enjal123 <68133433+enjal123@users.noreply.github.com>
2026-06-11 14:12:19 -0700

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for index, value in enumerate(nums):
+            missing = target - value
+
+            if missing in seen:
+                return [seen[missing], index]
+
+            seen[value] = index
+            
+        
